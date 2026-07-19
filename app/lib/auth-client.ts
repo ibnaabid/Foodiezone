b75cbe9ff00bd53e86a7daad0a92103e09f24d5d
MD Ibna Abid <mdabout2@gmail.com>
2026-07-19 22:04:12 +0600

    resolve auth.ts

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,8 +1,11 @@
 
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { auth } from "./auth";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL,
+    plugins: [inferAdditionalFields<typeof auth>()],
   
 });
 
